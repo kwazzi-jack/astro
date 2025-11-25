667f0a6e22f265e8bb1eebdd3af679e24840f73e
Brian Welman <brianallisterwelman@gmail.com>
2025-11-25 11:40:55 +0200

    py3.11->py3.12 to allow for better std typing (for now)

--- astro/__version__.py
+++ astro/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev42+g5fed7bf83.d20251125'
-__version_tuple__ = version_tuple = (0, 1, 'dev42', 'g5fed7bf83.d20251125')
+__version__ = version = '0.1.dev45+g9624ca1bf.d20251125'
+__version_tuple__ = version_tuple = (0, 1, 'dev45', 'g9624ca1bf.d20251125')
 
 __commit_id__ = commit_id = None
